2146a7bf5ef39dd73f54733923389ce0697bafa8
lammas@users.noreply.github.com <lammas@users.noreply.github.com>
2014-07-21 12:23:40 +0300

    Material now falls back to 1x1 white texture sampler if no samplers are bound

--- src/rendering/materials/Material.js
+++ src/rendering/materials/Material.js
@@ -3,7 +3,7 @@ var Material=Serializable.extend({
 	/** Constructor
 		@param shader Shader that will be used
 		@param uniforms Shader uniforms as object described in Shader.use
-		@param samplers Shader samplers as array described in Shader.bindSamplers/unbindSamplers 
+		@param samplers Shader samplers as array described in Shader.bindSamplers/unbindSamplers
 		@param descriptor MaterialDescriptor instance [optional] */
 	init: function(shader, uniforms, samplers, descriptor) {
 		this._super();
@@ -13,47 +13,59 @@ var Material=Serializable.extend({
 		this.uniforms=uniforms; // Shader uniforms as described by Shader
 		this.samplers=samplers; // Shader samplers
 	},
-	
+
 	type: function() {
 		return "Material";
 	},
-	
+
 	/** Binds material
 		@param uniforms Optional extra uniforms to go with the material
 		@param samplers Optional extra samplers to go with the material */
 	bind: function(uniforms, samplers) {
-		if(!this.shader) return;
-		
+		if (!this.shader) return;
+
 		this.shader.use(this.uniforms);
-		if(uniforms) this.shader.bindUniforms(uniforms);
-		if (samplers)
-			this.shader.bindSamplers(this.samplers.concat(samplers));
-		else 
-			this.shader.bindSamplers(this.samplers);
+		if (uniforms)
+			this.shader.bindUniforms(uniforms);
+
+		if ((!this.samplers || this.samplers.length == 0) && (!samplers || samplers.length == 0)) {
+			this.shader.bindSamplers([this.shader.context.engine.WhiteTextureSampler]);
+		}
+		else {
+			if (samplers)
+				this.shader.bindSamplers(this.samplers.concat(samplers));
+			else
+				this.shader.bindSamplers(this.samplers);
+		}
 	},
-	
+
 	/** Unbinds material
 		@param samplers Optional extra samplers to go with the material (the same that were passed to bind()) */
 	unbind: function(samplers) {
 		if(!this.shader) return;
-		
-		if (samplers)
-			this.shader.unbindSamplers(this.samplers.concat(samplers));
-		else
-			this.shader.unbindSamplers(this.samplers);
+
+		if ((!this.samplers || this.samplers.length == 0) && (!samplers || samplers.length == 0)) {
+			this.shader.unbindSamplers([this.shader.context.engine.WhiteTextureSampler]);
+		}
+		else {
+			if (samplers)
+				this.shader.unbindSamplers(this.samplers.concat(samplers));
+			else
+				this.shader.unbindSamplers(this.samplers);
+		}
 	},
-	
+
 	instantiate: function() {
 		var uniforms={};
 		for(var u in this.uniforms) {
 			uniforms[u]=this.uniforms[i].clone();
 		}
-	
+
 		var samplers=[];
 		for(var s in this.samplers) {
 			samplers.push(this.samplers[s].clone());
 		}
-	
+
 		var copy=new Material(this.shader, uniforms, samplers, this.descriptor);
 		copy.name=this.name+" (instance)";
 		return copy;
