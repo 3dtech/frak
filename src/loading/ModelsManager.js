3811417c39569a55ee427a6824a697b38faa9119
lammas@gmail.com <lammas@gmail.com>
2014-09-01 15:27:42 +0300

    Fixes a bug in ModelsManager

--- src/loading/ModelsManager.js
+++ src/loading/ModelsManager.js
@@ -4,32 +4,32 @@ var ModelsManager=Manager.extend({
 		this._super();
 		if(!shadersManager) shadersManager=new ShadersManager(context);
 		this.shadersManager=shadersManager;
-		
+
 		if(!texturesManager) texturesManager=new TexturesManager(context);
 		this.texturesManager=texturesManager;
-		
+
 		this.loadRelativeTextures=true;
 	},
-	
-	/** Gets loading progress. The progress per resource also depends on progress of 
+
+	/** Gets loading progress. The progress per resource also depends on progress of
 		shaders and textures manager used by models manager.
 		@return Current loading progress from 0 to 1 */
 	getProgress: function() {
 		if(this._super()==1.0) return 1.0;
 		return this._super()+(this.texturesManager.getProgress()+this.shadersManager.getProgress())/2.0/this.getTotalItems();
 	},
-	
+
 	add: function(source, noCollisionTree) {
 		return this.addDescriptor(new ModelDescriptor(source, noCollisionTree));
 	},
-	
+
 	createResource: function() {
 		return new Node();
 	},
-	
+
 	/** Must load given resource from location described by descriptor
-		@param descriptor Instance of resource descriptor 
-		@param resource Resource that will be loaded (created with createResource) 
+		@param descriptor Instance of resource descriptor
+		@param resource Resource that will be loaded (created with createResource)
 		@param loadedCallback Callback function(descriptor, resource) that must be called by loadResource when loading has finished successfully
 		@param failedCallback Callback function(descriptor) that must be called by loadResource when loading has failed */
 	loadResource: function(descriptor, resource, loadedCallback, failedCallback) {
@@ -37,7 +37,7 @@ var ModelsManager=Manager.extend({
 		var me=this;
 		Logistics.getBinary(descriptor.source,
 			function(binaryData) {
-				if(!binaryData) {
+				if(!binaryData || binaryData.byteLength == 0) {
 					failedCallback(descriptor);
 					return;
 				}
@@ -48,12 +48,12 @@ var ModelsManager=Manager.extend({
 						var delta = end-fileStartTime;
 						totalTime+=delta;
 						console.log('Parsed data: ', parsedData, " delta: ", delta, " time: ", totalTime);
-						
+
 						var modelLoader=new ModelLoader(me.context, descriptor, '', me.shadersManager, me.texturesManager, descriptor.noCollisionTree);
 						modelLoader.load(resource, parsedData);
-						
+
 						loadedCallback(descriptor, resource);
-						
+
 						me.shadersManager.load(function() {});
 						me.texturesManager.load(function() {});
 					},
