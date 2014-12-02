abbdc2730508b4c5a6b849b7030c1d595c52f098
lammas@gmail.com <lammas@gmail.com>
2014-12-02 17:44:58 +0200

    Shader compilation error messages now report the correct filename

--- src/rendering/shaders/FragmentShader.js
+++ src/rendering/shaders/FragmentShader.js
@@ -1,7 +1,11 @@
 /** FragmentShader for ShaderProgram. */
-var FragmentShader=Subshader.extend({ 
+var FragmentShader=Subshader.extend({
 	init: function(shader, code) {
 		this._super(shader, code, this.FRAGMENT_SHADER);
 		this.compiledShader=this.context.gl.createShader(this.context.gl.FRAGMENT_SHADER);
+	},
+
+	getFilename: function() {
+		return this.shader.descriptor.fragmentSource;
 	}
 });
--- src/rendering/shaders/VertexShader.js
+++ src/rendering/shaders/VertexShader.js
@@ -1,7 +1,11 @@
 /** VertexShader for ShaderProgram. */
-var VertexShader=Subshader.extend({ 
+var VertexShader=Subshader.extend({
 	init: function(shader, code) {
 		this._super(shader, code, this.VERTEX_SHADER);
 		this.compiledShader=this.context.gl.createShader(this.context.gl.VERTEX_SHADER);
+	},
+
+	getFilename: function() {
+		return this.shader.descriptor.vertexSource;
 	}
 });
